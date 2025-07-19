5a60fe8b956277b80e5d6aab0c6d57695e9cb015
Rani Ophir <raniop@mac.com>
2025-07-19 19:14:20 +0300

    Serve apple-app-site-association for Universal Links

--- server.js
+++ server.js
@@ -1,4 +1,3 @@
-
 const express = require('express');
 const path = require('path');
 
@@ -8,6 +7,11 @@ const PORT = process.env.PORT || 3000;
 app.get('/invite/:code', (req, res) => {
   const inviteCode = req.params.code;
   res.redirect(`quickteams://invite/${inviteCode}`);
+});
+
+app.get('/apple-app-site-association', (req, res) => {
+  res.setHeader('Content-Type', 'application/json');
+  res.sendFile(path.join(__dirname, 'apple-app-site-association'));
 });
 
 app.use(express.static(path.join(__dirname, 'public')));
